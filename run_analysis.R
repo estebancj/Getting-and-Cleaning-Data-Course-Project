ad5d11033078430b823a3042b6cdbe458da10070
Esteban Castillo <esteban.castillojz@udlap.mx>
2016-08-01 19:09:15 -0500

    New submission
    All the problems are fixed.

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,20 @@
+#if not install "plyr" then install the module
+if (!require("plyr")) {
+  install.packages(plyr)
+}
+
 #load plyr module 
 library(plyr)
-
-# script path
-# In order to run the script, change the path to your dataset folder
-setwd("C:/Users/EstebanCj/Desktop/coursera-getting-and-cleaning-data")
 
 #------Merges the training and the test sets to create one data set----
 
 #load the test and trainng parts of UCI HAR Dataset
-testA <- read.table("UCIHARDataset/test/X_test.txt")
-testB <- read.table("UCIHARDataset/test/y_test.txt")
-trainA <- read.table("UCIHARDataset/train/X_train.txt")
-trainB <- read.table("UCIHARDataset/train/y_train.txt")
-subjectTrain <- read.table("UCIHARDataset/train/subject_train.txt")
-subjectTest <- read.table("UCIHARDataset/test/subject_test.txt")
+testA <- read.table("./UCI HAR Dataset/test/X_test.txt")
+testB <- read.table("./UCI HAR Dataset/test/y_test.txt")
+trainA <- read.table("./UCI HAR Dataset/train/X_train.txt")
+trainB <- read.table("./UCI HAR Dataset/train/y_train.txt")
+subjectTrain <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+subjectTest <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 
 #merge the training and test dataset together(A,B and subject) 
 dataSetA <- rbind(trainA, testA)
@@ -24,21 +25,24 @@ dataSetSubject<- rbind(subjectTrain, subjectTest)
 #------Extracts only the measurements on the mean and standard deviation for each measurement----
 
 #extract the features names of the whole dataset
-features <- read.table("UCIHARDataset/features.txt")
+features <- read.table("./UCI HAR Dataset/features.txt")
 #extract the position (column number) of the mean and standard deviation metrics
-metrics <- grep("-(mean|std)\\(\\)", features[, 2])
+metrics <-grep(".*Mean.*|.*Std.*",x = features[, 2], ignore.case=TRUE)
 #obtain a subset of dataSetA that contain information about mean and standard deviation
 dataSetA <- dataSetA[,metrics]
 #extract the feature names of the mean and standard deviation metrics on dataSetA  and add to dataSetA
 names(dataSetA) <- features[metrics, 2]
+#eliminate extra symbols like [ ] ( ) , and extra spaces
+names(dataSetA) <- gsub('[-()]', ' ', names(dataSetA))
+names(dataSetA) <- gsub('\\s+', ' ', names(dataSetA))
+names(dataSetA) <- gsub(',', '', names(dataSetA))
 #-----------------------------------------------------------------------------------------------
 
 #------Uses descriptive activity names to name the activities in the data set-------
 
 #extract the activity names for activity_labels.txt
-activities <- read.table("UCIHARDataset/activity_labels.txt")
+activities <- read.table("./UCI HAR Dataset/activity_labels.txt")
 #remplace activity number with the activity names
-names(dataSetB)
 dataSetB[, 1] <- activities[dataSetB[, 1], 2]
 #-----------------------------------------------------------------------------------
 
@@ -51,11 +55,14 @@ names(dataSetSubject) <- "subject"
 #------creates a tidy data set with the average of each variable for each activity and each subject-----------
 #combine all datasets (A,B and Subject)
 datasets <- cbind(dataSetA, dataSetB, dataSetSubject)
+
 # independent tidy data set with the average of each variable for each activity and each subject.
-tidyData <- ddply(datasets, .(subject, activity), function(x) colMeans(x[, 1:66]))
+tidyData <- ddply(datasets, .(subject, activity), function(x) colMeans(x[, 1:86]))
 #-------------------------------------------------------------------------------------------------------------
 
 #write file
-write.table(tidyData, "tidyData.txt", row.name=TRUE)
+write.table(tidyData, "./tidyData.txt", row.name=FALSE)
+
+
 
 
